d36e7a629c0d5e22be70a61c97abba58a5ea510f
renzkk22 <69197456+renzkk22@users.noreply.github.com>
2021-02-13 15:42:07 -0300

    Add files via upload

---
+++ src/rojmenu.js
@@ -0,0 +1,12 @@
+const rojmenu = (prefix, pushname) => {
+    return `◪ *Comandos do ROJBOT*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}block
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}clearall`
+
+}
+
+exports.rojmenu = rojmenuk
